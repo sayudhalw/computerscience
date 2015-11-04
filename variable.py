c1ccbd7c07469d9d4c2f050c84ef9173e9c56f01
Sayudha Lukita Wibisana <sayudhagit@hotmail.com>
2015-11-04 21:37:44 +0700

    variabel dapat berubah

--- variable.py
+++ variable.py
@@ -1,5 +1,23 @@
-speed_of_light = 299792458  # metes per second
-cycles_per_second = 2700000000  # 2.7GHz
+#speed_of_light = 299792458  # metes per second
+#cycles_per_second = 2700000000  # 2.7GHz
 
-distance = speed_of_light / cycles_per_second
-print(distance)
+#distance = speed_of_light / cycles_per_second
+#print(distance)
+
+#-------------------------------------------------------------------------#
+
+#variabel bisa berubah2 tergantung kondisi
+#contoh :
+#What is the value of hours after running this code:
+#hours = 9
+#hours = hours + 1
+#hours = hours * 2
+
+hours = 9
+print(hours) #output = 9
+
+hours = hours + 1
+print(hours) #output = 10 (this is from hours = 9 and then + 1)
+
+hours = hours * 2
+print(hours) #output = 20 (thisi is from hours = 9 + 1 then * 2)
