013bd0b631c3055678a8bfd60cc3d7342d407429
Sayudha Lukita Wibisana <sayudhagit@hotmail.com>
2015-11-04 21:26:53 +0700

    change to DIVIDE

--- variable.py
+++ variable.py
@@ -1,5 +1,5 @@
 speed_of_light = 299792458  # metes per second
 cycles_per_second = 2700000000  # 2.7GHz
 
-distance = speed_of_light * cycles_per_second
+distance = speed_of_light / cycles_per_second
 print(distance)
